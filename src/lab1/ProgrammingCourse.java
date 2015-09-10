ebdb70f11a72ce0d6591f41aca0fd62026e0f090
darnell <darnell@L11721043664.wctc.msft>
2015-09-10 16:29:33 -0500

    Initial Commit

---
+++ src/lab1/ProgrammingCourse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author darnell
+ */
+public abstract class ProgrammingCourse {    
+    public String courseName;
+    public String courseNumber;
+    public double credits;
+    
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+    
+}
